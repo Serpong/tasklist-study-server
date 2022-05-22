6083657b2e29c78e93e1af47b4fdb948945bda23
Taeyeop Kim <osa1230617@gmail.com>
2022-05-22 11:01:16 +0900

    debug log

--- utils/responseUtil.js
+++ utils/responseUtil.js
@@ -8,8 +8,10 @@ module.exports.validationErrorFormatter = ({ location, msg, param, value, nested
 	return {errorType:"validation", param, msg};//errorFormatter({errorType:"validation", param, msg});
 };
 module.exports.responseSuccess = (res, data, statusCode)=>{
+	console.log(statusCode??200, data);
 	return res.status(statusCode??200).json(data);
 };
 module.exports.responseError = (res, error, statusCode)=>{
+	console.log(statusCode??400, error);
 	return res.status(statusCode??400).json({error:error});
 };
